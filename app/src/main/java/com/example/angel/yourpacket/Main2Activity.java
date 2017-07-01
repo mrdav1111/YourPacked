6598a95ba439fe321b512a0d6cc1a4260359af42
Angel <angeldavidn13@gmail.com>
2017-06-30 20:30:42 -0400

    prueba de branch

--- app/src/main/java/com/example/angel/yourpacket/Main2Activity.java
+++ app/src/main/java/com/example/angel/yourpacket/Main2Activity.java
@@ -52,7 +52,7 @@ public class Main2Activity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar  = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
